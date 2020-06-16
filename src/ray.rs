e5a621c03aa68699d0dce74c1017b7bc23851fa9
emrsmsrli <emrsmsrli1@gmail.com>
2020-06-16 13:33:54 +0300

    basic ray class impl

---
+++ src/ray.rs
@@ -0,0 +1,16 @@
+pub use nalgebra::{Vector3, Point3};
+
+pub struct Ray {
+    pub origin: Point3<f64>,
+    pub direction: Vector3<f64>
+}
+
+impl Ray {
+    pub fn at(&self, t: f64) -> Point3<f64> {
+        &self.origin + &self.direction * t
+    }
+
+    pub fn new(origin: Point3<f64>, direction: Vector3<f64>) -> Ray {
+        Ray { origin, direction }
+    }
+}
